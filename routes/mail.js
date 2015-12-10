fbfd38057c548b7454e0d590a426715daa95af85
András Iványi <andras.ivanyi@logmein.com>
2015-12-10 17:41:17 +0100

    Fixed hour display.

--- routes/mail.js
+++ routes/mail.js
@@ -52,6 +52,6 @@ var _getEmailTexts = (type, todos) => {
     "use strict";
     var br = type == 'html' ? "<br />" : '\n' ;
     var todoList = todos.join(`${br} - [ ] `);
-    var date = moment().format('h:mm MM.DD.YYYY');
+    var date = moment().format('HH:mm MM.DD.YYYY');
     return `Here is your meeting note created at ${date}.${br}${br}  [ ] ${todoList}`;
 };
